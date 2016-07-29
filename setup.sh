3e2044f7a51e9e9abb3e6f2473f540e7874ea6f2
Lisa Stephens <lisa.stephens@returnpath.com>
2016-07-28 22:08:48 -0600

    add mac locking fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ln -s "$(pwd)"/bash_profile ~/.bash_profile
 ln -s "$(pwd)"/git-completion.bash ~/git-completion.bash
+clang -framework Foundation "$(pwd)"/lockmac.m -o lockscreen
 . ~/.bash_profile
